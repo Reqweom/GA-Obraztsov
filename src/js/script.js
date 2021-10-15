aecd0f33a057bb3a5e08cc02065859df2a64cdf2
Reqweom <48221983+Reqweom@users.noreply.github.com>
2021-10-15 03:14:10 +0300

    processing project

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
-console.log('Start Project');
+const burger = document.querySelector('#burger');
+const burgerMenu = document.querySelector('.nav__menu')
+let menuOpen = false;
+
+(function (){
+    burgerMenu.classList.remove('nav__menu--active');
+    burger.classList.remove('burger--active');
+}());
+
+burger.addEventListener('click', (e)=>{
+    e.preventDefault();
+    if(!menuOpen){
+        burger.classList.add('burger--active');
+        burgerMenu.classList.add('nav__menu--active');
+        menuOpen = true;
+    }
+    else{
+        burger.classList.remove('burger--active');
+        burgerMenu.classList.remove('nav__menu--active');
+        menuOpen = false;
+    }
+})
